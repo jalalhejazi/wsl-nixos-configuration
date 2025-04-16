b49515ee9eb9a55ec1762b0151ea4f939a4d7f1b
jalal hejazi <jalal.hejazi@gmail.com>
2025-04-16 13:32:15 -0700

    enable git integration after the build

--- rebuild.sh
+++ rebuild.sh
@@ -3,12 +3,8 @@
 ## make the script executable
 ## chmod +x rebuild.sh
 
-## This script is used to rebuild the NixOS configuration.
-sudo nixos-rebuild switch --flake ~/configuration
-
 
 ## if param is passed to git, it will be used as the commit message and push
-
 if [ "$1" ]; then
     git add . 
     git commit -m "$1"
@@ -16,3 +12,9 @@ if [ "$1" ]; then
 else
     echo "No commit message provided. Skipping git commit and push."
 fi
+
+
+## This script is used to rebuild the NixOS configuration.
+sudo nixos-rebuild switch --flake ~/configuration
+
+
