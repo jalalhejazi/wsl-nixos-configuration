d49e03984df5aa760a9a50c67cd9689b2fbe5942
jalal hejazi <jalal.hejazi@gmail.com>
2025-04-16 13:28:02 -0700

    simplify the nixOS build

---
+++ rebuild.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+## make the script executable
+## chmod +x rebuild.sh
+
+## This script is used to rebuild the NixOS configuration.
+sudo nixos-rebuild switch --flake ~/configuration
