7f7c4166f95639a6659bc917d4f1b6442ca62503
jalal hejazi <jalal.hejazi@gmail.com>
2025-04-26 01:53:30 +0200

    2025-04-26 01:53:30 Automated git push

--- bash/git-push.sh
+++ bash/git-push.sh
@@ -2,9 +2,9 @@
 
 ## make the script executable
 ## chmod +x git-push.sh
-
+NOWT=$(date +"%Y-%m-%d %T")
 git add .
-git commit -m "NixOS | configuration update"
+git commit -m "$NOWT Automated git push"
 git push origin main
 
 
